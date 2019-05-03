cec8bb004dedf44ebd6e06da1452166051edcd00
Juliane Waack <jkwaack@gmail.com>
2019-05-03 14:19:01 +0200

    WIP desplay table for manual header tag validity checking

--- src/readers/WikiTablesReader.py
+++ src/readers/WikiTablesReader.py
@@ -1,13 +1,15 @@
 import os
 import itertools
 import json
+import plotly
 from pprint import pprint
 from definitions import PROJECT_ROOT_DIR
 
 
 class WikiTablesReader:
     def __init__(self):
-        self.wiki_tables_json = os.environ['WIKI_TABLES_JSON_DUMP']
+        #self.wiki_tables_json = os.environ['WIKI_TABLES_JSON_DUMP']
+        self.wiki_tables_json = os.path.join(PROJECT_ROOT_DIR, '../data/tables.json')
         self.output_dir = os.path.join(PROJECT_ROOT_DIR, 'data')
         self.tables = []
 
@@ -40,9 +42,41 @@ class WikiTablesReader:
                     results.append(table)
         return results
 
+    def displayTable(self, ordinal):
+        jsonTable = self.get(ordinal)
+        cellColors = []
+        textTable = []
+
+        headerRows = jsonTable['tableHeaders']
+        dataRows = jsonTable['tableData']
+        for row in (headerRows + dataRows):
+            rowColors = []
+            textRow = []
+            for cell in row:
+                text = cell['text']
+                if "Error: " in text:
+                    text = "Error: ..."
+                textRow.append(text)
+                html = cell['tdHtmlString']
+                if "<th " not in html:
+                    rowColors.append('#EDFAFF')
+                else:
+                    rowColors.append('#a1c3d1')
+            cellColors.append(rowColors)
+            textTable.append(textRow)
+        
+        plotTable = plotly.graph_objs.Table(
+            cells=dict(values=textTable,
+                line = dict(color='#7D7F80'),
+                fill = dict(color=cellColors))
+        )
+        data = [plotTable] 
+        plotly.plotly.iplot(data, filename = 'header_highlighted')
+        print("plotted table")
 
 if __name__ == '__main__':
     wtr = WikiTablesReader()
-    wtr.load_tables(5000)
-    pprint(wtr.get(55))
+    wtr.load_tables(60)
+    pprint(wtr.get(10))
     pprint(wtr.query_int('numHeaderRows', 5))
+    wtr.displayTable(10)
